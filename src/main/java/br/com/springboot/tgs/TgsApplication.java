a6f66f6aecfd270b663e8330f36eacacf2aff546
Caique <caiquepatelliescapeline@gmail.com>
2022-06-04 13:10:26 -0300

    V0
    create project

---
+++ src/main/java/br/com/springboot/tgs/TgsApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.tgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgsApplication.class, args);
+	}
+
+}
